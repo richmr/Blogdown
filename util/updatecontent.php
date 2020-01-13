fa79c5fe6bbda3cac36a98ccc2366369eeba5894
Mike <mike@tofet.net>
2020-01-12 22:36:02 -0800

    Update util/index

--- util/updatecontent.php
+++ util/updatecontent.php
@@ -3,7 +3,7 @@
     Uses git to fetch new changes to blogdown engine
   **********************/
 
-  $cmd = "git pull 2>&1";
+  $cmd = "cd ../../content; git pull 2>&1";
 
   echo '<pre>';
   passthru($cmd);
